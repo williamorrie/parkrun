73130b6e17c4dc08ef8b6b0ec65bfc8505e66122
William Orr <william@orr.ie>
2022-03-04 15:31:39 +0000

    Added icons and fixed geojson

--- index.js
+++ index.js
@@ -3,22 +3,35 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 2,
-    center: new google.maps.LatLng(54.875, -6.735),
+    center: new google.maps.LatLng(54.598, -5.929),
     mapTypeId: "terrain",
-    zoom: 9,
+    zoom: 11,
   });
+
+
   map.data.loadGeoJson(
       "./data.geojson"
   );
+
   map.data.setStyle(function(feature) {
-    const marker_base = "https://maps.google.com/mapfiles/ms/icons/"
+    //const marker_base = "https://maps.google.com/mapfiles/ms/icons/"
+    const marker_base = "https://maps.google.com/mapfiles/kml/shapes/"
     var ranking = parseInt(feature.getProperty('ranking'));
-    var marker_url = ranking == NaN ? marker_base + 'pink-dot.png' : 
-                     ranking < 250 ? marker_base + 'green-dot.png' : 
-                     ranking < 500 ? marker_base + 'orange-dot.png' : 
-                     marker_base + 'green-dot.png';
+    var marker_url = ranking == -1 ? marker_base + 'info_circle.png.png' : 
+                     ranking < 250 ? marker_base + 'hiker.png' : 
+                     ranking < 500 ? marker_base + 'police.png' : 
+                     marker_base + 'caution.png';
     return {
-      icon: marker_url
+      icon: marker_url,
+      clickable: true,
+      title: ranking
     };
+  });
+    // Set mouseover event for each feature.
+  map.data.addListener("mouseover", (event) => {
+    document.getElementById("info-box").textContent =
+      "URI " + + event.feature.getProperty("uri") +
+      "Ranking: " + event.feature.getProperty("ranking") +
+      "Index: " + event.feature.getProperty("index");
   });
 };
